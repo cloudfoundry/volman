77237ebd0a737bca2934dff37aa866bfe9d36bf5
Nagapramod Mandagere <pramod@us.ibm.com>
2016-01-25 14:11:08 -0800

    Solved failing test by adding a basic http server
    Signed-off-by: Luke Woydziak <luke.woydziak@emc.com>

--- cmd/volman/main.go
+++ cmd/volman/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	// cf_debug_server "github.com/cloudfoundry-incubator/cf-debug-server"
+	// cf_lager "github.com/cloudfoundry-incubator/cf-lager"
+)
+
+// var communicationTimeout = flag.Duration(
+// 	"communicationTimeout",
+// 	10*time.Second,
+// 	"Timeout applied to all HTTP requests.",
+// )
+
+func main() {
+	// cf_debug_server.AddFlags(flag.CommandLine)
+	// cf_lager.AddFlags(flag.CommandLine)
+	flag.Parse()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+	//cf_http.Initialize(*communicationTimeout)
+}
+
+// func initializeVolmanServer(runner volmantypes.Runner) ifrit.Runner {
+// 	return http_server.New(*listenAddr, handlers.New(runner))
+// }
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
