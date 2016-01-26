3754dfb7919d57d2da39dee3f4a5f53e88a99984
Luke Woydziak <luke.woydziak@emc.com>
2016-01-26 12:13:14 -0800

    Refactor main after adding debug server.
    Signed-off-by: Nagapramod Mandagere <pramod@us.ibm.com>

--- cmd/volman/main.go
+++ cmd/volman/main.go
@@ -23,45 +23,48 @@ var atAddress = flag.String(
 )
 
 func main() {
+	parseCommandLine()
 	withLogger, logTap := logger()
-	cf_debug_server.AddFlags(flag.CommandLine)
-	flag.Parse()
-
 	servers := grouper.Members{}
-	servers = append(server(*atAddress), servers...)
-
-	atDebugAddress := cf_debug_server.DebugAddress(flag.CommandLine)
-
-	if atDebugAddress != "" {
-		servers = append(debugServer(atDebugAddress, logTap), servers...)
-	}
-
-	processes := sigmon.New(grouper.NewOrdered(os.Interrupt, servers))
-
-	run(Invoke(processes), withLogger)
+	servers = createAndAppendServer(*atAddress, servers)
+	servers = createAndAppendDebugServer(cf_debug_server.DebugAddress(flag.CommandLine), logTap, servers)
+	untilTerminated(Invoke(processRunnerFor(servers)), withLogger)
 }
 
-func run(process Process, logger lager.Logger) {
-	logger.Info("started")
-	err := <-process.Wait()
+func exitOnFailure(err error) {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func untilTerminated(process Process, logger lager.Logger) {
+	logger.Info("started")
+	err := <-process.Wait()
+	exitOnFailure(err)
 	logger.Info("exited")
 }
 
-func debugServer(atDebugAddress string, logTap *lager.ReconfigurableSink) grouper.Members {
-	return grouper.Members{
-		{"debug-server", cf_debug_server.Runner(atDebugAddress, logTap)},
-	}
+func processRunnerFor(servers grouper.Members) Runner {
+	return sigmon.New(grouper.NewOrdered(os.Interrupt, servers))
 }
 
-func server(atAddress string) grouper.Members {
-	handler, _ := volmanHandlers()
-	server := http_server.New(atAddress, handler)
-	return grouper.Members{
-		{"volman-server", server},
+func createAndAppendDebugServer(atDebugAddress string, logTap *lager.ReconfigurableSink, servers grouper.Members) grouper.Members {
+	if atDebugAddress == "" {
+		return servers
 	}
+
+	return append(grouper.Members{
+		{"debug-server", cf_debug_server.Runner(atDebugAddress, logTap)},
+	}, servers...)
+}
+
+func createAndAppendServer(atAddress string, servers grouper.Members) grouper.Members {
+	handler, err := volmanHandlers()
+	exitOnFailure(err)
+	server := http_server.New(atAddress, handler)
+	return append(grouper.Members{
+		{"volman-server", server},
+	}, servers...)
 }
 
 func volmanHandlers() (http.Handler, error) {
@@ -78,7 +81,13 @@ func volmanHandlers() (http.Handler, error) {
 }
 
 func logger() (lager.Logger, *lager.ReconfigurableSink) {
-	cf_lager.AddFlags(flag.CommandLine)
+
 	logger, reconfigurableSink := cf_lager.New("volman")
 	return logger, reconfigurableSink
+}
+
+func parseCommandLine() {
+	cf_lager.AddFlags(flag.CommandLine)
+	cf_debug_server.AddFlags(flag.CommandLine)
+	flag.Parse()
 }
