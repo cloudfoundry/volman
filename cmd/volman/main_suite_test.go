4d114b9cec5e951bcb583d9790028560b0b20dba
Luke Woydziak <luke.woydziak@emc.com>
2016-03-29 15:50:28 -0700

    Parameterized the driver for the main_test to allow for other drivers to leverage the same test suite for integration.
    [#114241305](https://www.pivotaltracker.com/story/show/114241305)
    
    Signed-off-by: Nagapramod Mandagere <pramod@us.ibm.com>

--- cmd/volman/main_suite_test.go
+++ cmd/volman/main_suite_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os/exec"
-	"path"
 	"strings"
 	"testing"
 	"time"
@@ -12,29 +11,20 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
-	"github.com/tedsuo/ifrit"
 	"github.com/tedsuo/ifrit/ginkgomon"
 )
 
 var volmanPath string
 var volmanServerPort int
 var debugServerAddress string
-var volmanProcess ifrit.Process
-var runner *ginkgomon.Runner
+var volmanRunner *ginkgomon.Runner
 
-var fakeDriverPath string
-var fakedriverServerPort int
+var driverPath string
+var driverServerPort int
 var debugServerAddress2 string
-var fakedriverProcess ifrit.Process
-var fakedriverRunner *ginkgomon.Runner
+var driverRunner *ginkgomon.Runner
 
 var tmpDriversPath string
-
-var fakedriverServerPath string
-
-var unixRunner *ginkgomon.Runner
-var fakedriverUnixServerProcess ifrit.Process
-var socketPath string
 
 func TestVolman(t *testing.T) {
 	// these integration tests can take a bit, especially under load;
@@ -50,13 +40,13 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	volmanPath, err = gexec.Build("github.com/cloudfoundry-incubator/volman/cmd/volman", "-race")
 	Expect(err).NotTo(HaveOccurred())
 
-	fakeDriverPath, err = gexec.Build("github.com/cloudfoundry-incubator/volman/fakedriver/cmd/fakedriver", "-race")
+	driverPath, err = gexec.Build("github.com/cloudfoundry-incubator/volman/fakedriver/cmd/fakedriver", "-race")
 	Expect(err).NotTo(HaveOccurred())
-	return []byte(strings.Join([]string{volmanPath, fakeDriverPath}, ","))
+	return []byte(strings.Join([]string{volmanPath, driverPath}, ","))
 }, func(pathsByte []byte) {
 	path := string(pathsByte)
 	volmanPath = strings.Split(path, ",")[0]
-	fakeDriverPath = strings.Split(path, ",")[1]
+	driverPath = strings.Split(path, ",")[1]
 })
 
 var _ = BeforeEach(func() {
@@ -64,13 +54,13 @@ var _ = BeforeEach(func() {
 	tmpDriversPath, err = ioutil.TempDir("", "driversPath")
 	Expect(err).NotTo(HaveOccurred())
 
-	fakedriverServerPort = 9750 + GinkgoParallelNode()
+	driverServerPort = 9750 + GinkgoParallelNode()
 	debugServerAddress2 = fmt.Sprintf("0.0.0.0:%d", 9850+GinkgoParallelNode())
-	fakedriverRunner = ginkgomon.New(ginkgomon.Config{
+	driverRunner = ginkgomon.New(ginkgomon.Config{
 		Name: "fakedriverServer",
 		Command: exec.Command(
-			fakeDriverPath,
-			"-listenAddr", fmt.Sprintf("0.0.0.0:%d", fakedriverServerPort),
+			driverPath,
+			"-listenAddr", fmt.Sprintf("0.0.0.0:%d", driverServerPort),
 			"-debugAddr", debugServerAddress2,
 		),
 		StartCheck: "fakedriverServer.started",
@@ -78,7 +68,7 @@ var _ = BeforeEach(func() {
 
 	volmanServerPort = 8750 + GinkgoParallelNode()
 	debugServerAddress = fmt.Sprintf("0.0.0.0:%d", 8850+GinkgoParallelNode())
-	runner = ginkgomon.New(ginkgomon.Config{
+	volmanRunner = ginkgomon.New(ginkgomon.Config{
 		Name: "volman",
 		Command: exec.Command(
 			volmanPath,
@@ -88,25 +78,6 @@ var _ = BeforeEach(func() {
 		),
 		StartCheck: "volman.started",
 	})
-
-	socketPath = path.Join(fakeDriverPath, "fakedriver_unix.sock")
-
-	unixRunner = ginkgomon.New(ginkgomon.Config{
-		Name: "fakedriverUnixServer",
-		Command: exec.Command(
-			fakeDriverPath,
-			"-listenAddr", socketPath,
-			"-transport", "unix",
-		),
-		StartCheck: "fakedriverUnixServer.started",
-	})
-
-})
-
-var _ = AfterEach(func() {
-	ginkgomon.Kill(volmanProcess)
-	ginkgomon.Kill(fakedriverProcess)
-	ginkgomon.Kill(fakedriverUnixServerProcess)
 })
 
 var _ = SynchronizedAfterSuite(func() {
