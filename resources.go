f17453c8c15db4a6cfca3d707f25b476f0d09d50
Julian Hjortshoj <julian.hjortshoj@emc.com>
2017-04-25 17:15:14 -0700

    remove unused scripts, main.go, and remote client code from volman...tidiness is king. [#142360675](https://www.pivotaltracker.com/story/show/142360675)

--- resources.go
+++ resources.go
@@ -1,19 +1,5 @@
 package volman
 
-import "github.com/tedsuo/rata"
-
-const (
-	ListDriversRoute = "drivers"
-	MountRoute       = "mount"
-	UnmountRoute     = "unmount"
-)
-
-var Routes = rata.Routes{
-	{Path: "/drivers", Method: "GET", Name: ListDriversRoute},
-	{Path: "/drivers/mount", Method: "POST", Name: MountRoute},
-	{Path: "/drivers/unmount", Method: "POST", Name: UnmountRoute},
-}
-
 type ListDriversResponse struct {
 	Drivers []InfoResponse `json:"drivers"`
 }
@@ -35,16 +21,4 @@ type InfoResponse struct {
 type UnmountRequest struct {
 	DriverId string `json:"driverId"`
 	VolumeId string `json:"volumeId"`
-}
-
-func NewError(err error) Error {
-	return Error{err.Error()}
-}
-
-type Error struct {
-	Description string `json:"description"`
-}
-
-func (e Error) Error() string {
-	return e.Description
 }
